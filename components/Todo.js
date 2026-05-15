a2399dba703fed45e6a8cc0930eef73de5bc200e
mfprojects355 <mohfar081@gmail.com>
2026-05-15 13:41:32 +0400

    project complete

---
+++ components/Todo.js
@@ -0,0 +1,62 @@
+class Todo {
+  constructor(data, selector) {
+    this._data = data;
+    this._templateElement = document.querySelector(selector);
+  }
+
+  _createViewElement() {
+    const todoElement = this._templateElement.content
+      .querySelector(".todo")
+      .cloneNode(true);
+
+    this._nameEl = todoElement.querySelector(".todo__name");
+    this._checkboxEl = todoElement.querySelector(".todo__completed");
+    this._labelEl = todoElement.querySelector(".todo__label");
+    this._dateEl = todoElement.querySelector(".todo__date");
+    this._deleteBtn = todoElement.querySelector(".todo__delete-btn");
+
+    return todoElement;
+  }
+
+  _setName() {
+    this._nameEl.textContent = this._data.name;
+  }
+
+  _setCheckbox() {
+    this._checkboxEl.checked = this._data.completed;
+    this._checkboxEl.id = `todo-${this._data.id}`;
+    this._labelEl.setAttribute("for", `todo-${this._data.id}`);
+  }
+
+  _setDueDate() {
+    const dueDate = new Date(this._data.date);
+    if (!isNaN(dueDate)) {
+      this._dateEl.textContent = `Due: ${dueDate.toLocaleString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}`;
+    }
+  }
+
+  _setEventListeners() {
+    this._deleteBtn.addEventListener("click", () => {
+      this._element.remove();
+    });
+
+    this._checkboxEl.addEventListener("change", () => {
+      this._data.completed = this._checkboxEl.checked;
+    });
+  }
+
+  getView() {
+    this._element = this._createViewElement();
+    this._setName();
+    this._setCheckbox();
+    this._setDueDate();
+    this._setEventListeners();
+    return this._element;
+  }
+}
+
+export default Todo;
